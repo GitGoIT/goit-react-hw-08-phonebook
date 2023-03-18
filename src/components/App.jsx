e556d798c78e579e38cc8d9215a0d47c79e4fe67
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-03-18 11:02:20 +0200

    commit_finaly-upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store, persistor } from '../redux/store';
 import { Navbar } from './Navbar/Navbar.jsx';
 import { UserRoutes } from './UserRoutes';
-import { PersistGate } from 'redux-persist/integration/react';
-import { AuthLayout } from './AuthLayout/AuthLayout';
+
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <AuthLayout>
-          <BrowserRouter>
-            <Navbar />
-            <UserRoutes />
-          </BrowserRouter>
-        </AuthLayout>
-      </PersistGate>
-    </Provider>
+    <>
+      <Navbar />
+      <UserRoutes />
+    </>
   );
 }
 
